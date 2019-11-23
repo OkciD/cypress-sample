e2d8873b064a09a0742ce10025cc3c63e0ff33a9
Yury Baykov <y.baykov@corp.mail.ru>
2019-11-23 03:09:12 +0300

    first serious test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,25 @@
 describe('Sample page', () => {
-	it('successfully loads', () => {
+	before(() => {
 		cy.visit('/');
+	});
+
+	it('successfully loads', () => {
 		cy.get('body').should('be.visible');
+	});
+
+	it('add elements to list on submit', () => {
+		cy.get('[data-test-id="input"]')
+			.type('kek')
+			.should('have.value', 'kek');
+
+		cy.get('[data-test-id="button"]')
+			.click();
+
+		cy.get('[data-test-id="list"]')
+			.children()
+			.contains('kek');
+
+		cy.get('[data-test-id="input"]')
+			.should('not.have.value');
 	});
 });
